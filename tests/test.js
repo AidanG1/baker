fed86b0ec868376357a7b7619965f9346a7c5018
Aidan Gerber <agentaidan@gmail.com>
2022-04-09 01:47:20 -0500

    add tests, improve nav

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,12 @@
 import { expect, test } from '@playwright/test';
+// npx playwright test
 
 test('index page has expected h1', async ({ page }) => {
 	await page.goto('/');
-	expect(await page.textContent('h1')).toBe('Welcome to the Best College');
+	expect(await page.textContent('h1')).toBe('Welcome to Baker');
+});
+
+test('history page starts with', async ({ page }) => {
+	await page.goto('/history');
+	expect(await page.textContent('h2')).toBe(' Behind the First residential college on Rice University... 🔗');
 });
