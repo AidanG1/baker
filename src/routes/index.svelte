97e4e157292032acb4657c52a9ae307936ee0625
Aidan Gerber <agentaidan@gmail.com>
2022-04-08 21:50:40 -0500

    confetti instead of get started button

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,18 +1,31 @@
 <script>
 	import Calendar from '../components/Calendar.svelte';
 	import Map from '../components/Map.svelte';
+	import { ConfettiExplosion } from 'svelte-confetti-explosion';
+	import { tick } from 'svelte';
+	let isVisible = false;
+	async function visibility() {
+		isVisible = false;
+		await tick();
+		isVisible = true;
+	}
 </script>
 
+{#if isVisible}
+	<div id="confetti" class="justify-center flex">
+		<ConfettiExplosion colors={['#862616', '#f2b30c', '#b3baba', '#040404', '#b3baba']} />
+	</div>
+{/if}
 <div class="hero min-h-screen" style="background-image: url(bg-light-theme.svg);">
 	<div class="hero-overlay bg-opacity-60" />
 	<div class="hero-content text-center bg-base-100">
 		<div class="max-w-md">
-			<h1 class="mb-5 text-5xl font-bold">Welcome to the Best College</h1>
+			<h1 class="mb-5 text-5xl font-bold">Welcome to Baker</h1>
 			<p class="mb-5">
 				Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
 				quasi. In deleniti eaque aut repudiandae et a id nisi.
 			</p>
-			<button class="btn btn-primary">Get Started</button>
+			<button class="btn btn-primary" on:click={visibility}>Celebrate!</button>
 		</div>
 	</div>
 </div>
@@ -21,13 +34,3 @@
 <div class="flex justify-center">
 	<Map />
 </div>
-<!-- <iframe
-	src="https://calendar.google.com/calendar/embed?src=c_99gthh6cl5ktjjjc6hnuu4p1k8%40group.calendar.google.com&ctz=America%2FChicago"
-	style="border: 0"
-	width="800"
-	height="600"
-	frameborder="0"
-	scrolling="no"
-    title="Baker Google Calendar"
-    referrerpolicy="no-referrer"
-/> -->
